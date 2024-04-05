214bab8562aaebc06e5747b5b7417fcad7772b16
xxbrendenxx <155623716+xxbrendenxx@users.noreply.github.com>
2024-04-05 08:25:21 -0500

    Initialize main

---
+++ Example/ContentView.swift
@@ -0,0 +1,17 @@
+import Web3Modal
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
